09224f09a38704f3aea487745b615c3b229b7b58
GenRubio <burbianio@gmail.com>
2024-01-01 16:41:21 +0100

    WIP :: Update

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,14 +3,14 @@ import { useAuth } from '../store/auth.store';
 export default async function AuthMiddleware(to, from, next) {
     const auth = useAuth();
     if (to.meta?.auth) {
-        if (to.name == "Game" && !auth.isAuth) {
+        if (to.name == "Game" && !await auth.checkToken()) {
             next({ name: "Auth" });
         }
         else {
             next();
         }
     } else {
-        if (to.name == "Auth" && auth.isAuth) {
+        if (to.name == "Auth" && await auth.checkToken()) {
             next({ name: "Game" });
         }else{
             next();
